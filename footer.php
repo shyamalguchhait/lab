d93e6350ae14ceb53329b450cde62b9d39003f30
Shyamal <guchhait2016@gmail.com>
2021-10-28 03:13:04 +0530

    menu

--- footer.php
+++ footer.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-    .footer{
-        position: fixed;
-        left: 0;
-        bottom:0;
-        width: 100%;
-        text-align: center;
-        background-color: red;
-        color: white;
-        height: 30px;
-        font-family: "Comic Sans MS";
-        font-size: large;
-        padding-top: 0px;
-    }
-    @media screen and (max-width: 900px){
-        .footer{font-size: 10px;}
-    }
-</style>
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            background-color: red;
+            color: white;
+            height: 30px;
+            font-family: "Comic Sans MS";
+            font-size: large;
+            padding-top: 0px;
+            z-index: 99;
+        }
+
+        @media screen and (max-width: 900px) {
+            .footer {
+                font-size: 10px;
+            }
+        }
+    </style>
 </head>
+
 <body>
     <div class="footer">
         &#169 2020 Webpage designed by Bio-optics & nano-photonics group of IISER kolkata.
     </div>
 </body>
+
 </html>
