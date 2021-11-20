4c1735f4dcde49291eaed2bc1b820333b1628acd
shyamal <guchhait2016@gmail.com>
2021-11-20 23:53:56 +0530

    member menu

--- member/menu.php
+++ member/menu.php
@@ -1,33 +1,19 @@
-<!--<!DOCTYPE html>-->
-<html>
-
-<head>
-    <!-- CSS only -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="IISER Kolkata BioNaP group official webpage">
-    <meta name="keywords" content="Research, Publication, Member, Visitor, Collaborator">
-    <meta name="author" content="">
-    <title></title>
-    <link href="../static/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <script src="../static/bootstrap-5.0.2-dist/popperjs/popper.min.js" type="text/javascript"></script>
-    <script src="../static/bootstrap-5.0.2-dist/popperjs/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
-    <script src="../static/bootstrap-5.0.2-dist/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../static/bootstrap-5.0.2-dist/carhartl-jquery-cookie/jquery.cookie.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="../static/css/menu.css" type="text/css">
-    <script src="../static/js/menu.js" type="text/javascript"></script>
-</head>
-
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="../index.php">
-                <img src="../image/logo.png" alt="logo">
+                <img src="./image/logo.png" alt="logo">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler ml-auto custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="navbar-brand">
+                <div class="switch">
+                    <input class="toggle" type="checkbox" id="switch" name="mode">
+                    <label for="switch"> Toggle </label>
+                </div>
+            </div>
+            
             <div class="d-flex flex-row-reverse">
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav mr-auto">
@@ -74,13 +60,11 @@
                     </ul>
                 </div>
             </div>
-            <div class="switch">
-                <input class="toggle" type="checkbox" id="switch" name="mode">
-                <label for="switch"> Toggle </label>
-            </div>
         </div>
     </nav>
+    <script src="../static/bootstrap-5.0.2-dist/carhartl-jquery-cookie/jquery.cookie.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="../static/css/menu.css" type="text/css">
+    <script src="../static/js/menu.js" type="text/javascript"></script>
 </body>
 
-</html>
-<?php include 'footer.php'; ?>
+<?php include '../footer.php'; ?>
